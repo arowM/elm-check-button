ac91a6b76c735d575768ea61d5625588ca27f4f9
Kadzuya Okamoto <arow.okamoto@gmail.com>
2017-01-01 17:19:47 +0900

    Add initial source.

---
+++ src/CheckButton.elm
@@ -0,0 +1,235 @@
+module CheckButton
+  exposing
+    ( Model
+    , Namespace
+    , Value
+    , defaultModel
+    , setChecked
+    , setUnchecked
+    , setIsChecked
+    , setActive
+    , setDisabled
+    , setLabel
+    , view
+    , css
+    )
+
+{-| A button component that can be checked or unchecked.
+  This module also expose a function to generate CSS
+  in manner of [`elm-css`](https://github.com/rtfeldman/elm-css).
+
+# Model
+
+@docs Model
+
+## Constructors
+
+@docs defaultModel
+
+## Modifiers
+
+@docs setChecked
+@docs setUnchecked
+@docs setIsChecked
+@docs setActive
+@docs setDisabled
+@docs setLabel
+
+# Renderers
+
+@docs view
+
+# CSS
+
+@docs css
+
+# Aliases
+@docs Namespace
+@docs Value
+
+-}
+
+import Css exposing (Stylesheet)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.CssHelpers as CssHelpers exposing (withNamespace)
+import Html.Events exposing (..)
+import CheckButton.Css as Css exposing (CssClasses(..))
+
+
+-- Exposed aliases
+
+
+{-| An alias for readability
+-}
+type alias Namespace =
+  String
+
+
+{-| An alias for readability
+-}
+type alias Value =
+  String
+
+
+
+-- Model
+
+
+{-| An opaque type representing a check button state.
+  To construct an actual value of this type, use constructors and modifiers bellow.
+
+  Example usage.
+
+  button =
+    defaultModel "red" "Red"
+    |> setChecked
+    |> setDisabled
+
+-}
+type Model
+  = CheckButton
+    { value : Value
+    , label : String
+    , isChecked : Bool
+    , isActive : Bool
+    }
+
+
+
+-- Constructors
+
+
+{-| A constructor for a check button.
+  By providing a value and a label name, you can construct an unchecked active button.
+-}
+defaultModel : Value -> String -> Model
+defaultModel value label =
+  CheckButton
+    { value = value
+    , label = label
+    , isChecked = False
+    , isActive = True
+    }
+
+
+{-| Set the state of a check button to be checked.
+-}
+setChecked : Model -> Model
+setChecked =
+  setIsChecked True
+
+
+{-| Set the state of a check button to be unchecked.
+-}
+setUnchecked : Model -> Model
+setUnchecked =
+  setIsChecked False
+
+
+{-| Modify the state of a check button to be checked/unchecked.
+
+  setChecked = setIsChecked True
+  setUnchecked = setIsChecked False
+-}
+setIsChecked : Bool -> Model -> Model
+setIsChecked isChecked (CheckButton button) =
+  CheckButton
+    { button
+      | isChecked = isChecked
+    }
+
+
+{-| Set the state of a check button to be active.
+-}
+setActive : Model -> Model
+setActive =
+  setIsActive True
+
+
+{-| Set the state of a check button to be disabled.
+-}
+setDisabled : Model -> Model
+setDisabled =
+  setIsActive False
+
+
+{-| Modify the state of a check button to be active/disabled.
+  This is an inner function.
+
+  setActive = setIsActive True
+  setDisabled = setIsActive False
+-}
+setIsActive : Bool -> Model -> Model
+setIsActive isActive (CheckButton button) =
+  CheckButton
+    { button
+      | isActive = isActive
+    }
+
+
+{-| Set a check button label name.
+-}
+setLabel : String -> Model -> Model
+setLabel label (CheckButton button) =
+  CheckButton
+    { button
+      | label = label
+    }
+
+
+
+-- View
+
+
+{-| A renderer for check button.
+
+  The first argument is a name space for
+  [elm-css](https://github.com/rtfeldman/elm-css).
+  It supposed to be a unique value.
+
+  The second argument is called if a user checked/unchecked a check button.
+-}
+view : Namespace -> (Bool -> msg) -> Model -> Html msg
+view namespace onToggle (CheckButton checkButton) =
+  let
+    { id, class, classList } =
+      withNamespace namespace
+
+    { value, isChecked } =
+      checkButton
+
+    uniqueId =
+      namespace ++ "-" ++ value
+  in
+    div
+      [ class [ CheckButton ]
+      ]
+      [ input
+        [ type_ "checkbox"
+        , id uniqueId
+        , class [ CheckButtonCore ]
+        , name namespace
+        , checked isChecked
+        , onCheck onToggle
+        ]
+        []
+      , label
+        [ for uniqueId
+        , class [ CheckButtonLabel ]
+        ]
+        [ text checkButton.label
+        ]
+      ]
+
+
+
+-- CSS
+
+
+{-| A CSS component.
+Please reffer to the [elm-css](https://github.com/rtfeldman/elm-css) for detail.
+-}
+css : CssHelpers.Namespace String class id msg -> Stylesheet
+css =
+  Css.css
