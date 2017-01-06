7cee7b4113a2797725161604827df3e1eaf8e3cf
Kadzuya Okamoto <arow.okamoto@gmail.com>
2017-01-06 22:07:38 +0900

    Add comments on CheckButtons.Css.

--- src/CheckButtons/Css.elm
+++ src/CheckButtons/Css.elm
@@ -4,15 +4,29 @@ module CheckButtons.Css
     , CssClasses(..)
     )
 
+{-| This module expose a function to generate CSS
+  for `CheckButtons` in a manner of [`elm-css`](https://github.com/rtfeldman/elm-css).
+
+# CSS
+
+@docs css
+@docs CssClasses
+-}
+
 import Css exposing (..)
 import Css.Namespace exposing (namespace)
 import Html.CssHelpers
 
 
+{-| A CSS classes.
+-}
 type CssClasses
   = ButtonSet
 
 
+{-| A CSS component.
+Please reffer to the [elm-css](https://github.com/rtfeldman/elm-css) for detail.
+-}
 css : List Snippet
 css =
     [ everything
